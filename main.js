c8515182ea30360a9f9131dc8c06726a68470a27
rettusm <rettusm@wwu.edu>
2021-06-25 13:31:41 -0700

    event handling

--- main.js
+++ main.js
@@ -1,11 +1,29 @@
 const app = Vue.createApp({
     data() {
         return {
+        cart: 0,
         product: 'Socks',
-        image: './assets/vmSocks-green-onWhite.jpg',
+        image: './assets/images/socks_green.jpg',
+        url: 'https://www.vuemastery.com/',
         inventory: 8,
         onSale: true,
-        details: ["80% cotton", "20% polyester", "Gender-neutral"]
+        details: ["80% cotton", "20% polyester", "Gender-neutral"],
+        variants: [
+            { id: 2234, color: 'green', image: './assets/images/socks_green.jpg' },
+            { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg' }
+        ],
+        sizes: ["small", "medium", "large"]
+        }
+    },
+    methods: {
+        addToCart() {
+            this.cart += 1
+        },
+        takeFromCart() {
+            this.cart -= 1
+        },
+        updateImage(variantImage){
+            this.image = variantImage
         }
     }
 })
