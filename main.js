b92dbbbc5961f9f935cf38e213a8359027a410b4
rettusm <rettusm@wwu.edu>
2021-06-25 12:08:32 -0700

    vue3

--- main.js
+++ main.js
@@ -1,10 +1,11 @@
-var app = new Vue({
-    el: '#app',
-    data: {
+const app = Vue.createApp({
+    data() {
+        return {
         product: 'Socks',
         image: './assets/vmSocks-green-onWhite.jpg',
         inventory: 8,
         onSale: true,
         details: ["80% cotton", "20% polyester", "Gender-neutral"]
+        }
     }
 })
